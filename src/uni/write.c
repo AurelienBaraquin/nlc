e46b62990901ea3710a57595b6aa479fa149b037
AurelienBaraquin <aurelien.baraquin@epitech.eu>
2024-04-15 12:44:01 +0200

    Add input validation to write function

--- src/uni/write.c
+++ src/uni/write.c
@@ -1,6 +1,9 @@
 #include "uninlc.h"
 
 int write(int fd, const void *buf, size_t count) {
+    if (fd < 0 || buf == (void *)0)
+        return -1;
+
     int ret;
     asm volatile (
         "syscall"
