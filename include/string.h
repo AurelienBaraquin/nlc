58c4853f6d560bec8b8992882a97561e2f8ec66a
AurelienBaraquin <aurelien.baraquin@epitech.eu>
2024-04-16 11:39:49 +0200

    Add size_t typedef and memory functions

--- include/string.h
+++ include/string.h
@@ -1,3 +1,10 @@
 #pragma once
 
-long unsigned int strlen(const char *s);
+typedef long unsigned int size_t;
+
+// string
+size_t strlen(const char *s);
+
+// memory
+void *memset(void *s, int c, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
---
+++ src/string/mem.c
@@ -0,0 +1,18 @@
+#include "string.h"
+
+void *memset(void *s, int c, size_t n) {
+    unsigned char *p = s;
+    while (n--) {
+        *p++ = (unsigned char)c;
+    }
+    return s;
+}
+
+void *memcpy(void *dest, const void *src, size_t n) {
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
