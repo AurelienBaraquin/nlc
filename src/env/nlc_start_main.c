d1aebc14363e892195d0d6592f1da20b41fade3d
AurelienBaraquin <aurelien.baraquin@epitech.eu>
2024-07-14 11:27:56 +0200

    Refactor __nlc_start_main to include memory management functions and error handling

--- src/env/nlc_start_main.c
+++ src/env/nlc_start_main.c
@@ -1,4 +1,5 @@
 extern void exit(int status);
+void *sbrk(void *increment);
 extern void __nlc_csu_init(void);
 extern void __nlc_csu_fini(void);
 
@@ -8,6 +9,10 @@ __attribute__((force_align_arg_pointer))
 int __nlc_start_main(int (*main)(int,char **,char **), int argc, char **argv)
 {
 	char **envp = argv + argc + 1;
+
+    void *ptr = sbrk(0);
+    if (ptr == (void *)-1)
+        exit(1);
 
     __nlc_csu_init();
 
