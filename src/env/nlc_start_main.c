d38e403235e06e040db87dff0b1659d140ca8752
AurelienBaraquin <aurelien.baraquin@epitech.eu>
2024-07-14 15:08:22 +0200

    chore: Update sbrk function declaration to include extern keyword

--- src/env/nlc_start_main.c
+++ src/env/nlc_start_main.c
@@ -1,9 +1,7 @@
 extern void exit(int status);
-void *sbrk(void *increment);
+extern void *sbrk(void *increment);
 extern void __nlc_csu_init(void);
 extern void __nlc_csu_fini(void);
-
-int main(int argc, char *argv[], char *envp[]);
 
 __attribute__((force_align_arg_pointer))
 int __nlc_start_main(int (*main)(int,char **,char **), int argc, char **argv)
